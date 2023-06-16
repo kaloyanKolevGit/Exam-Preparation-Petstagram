af98289ee5c42d622672d7d80e8f6fc409fce989
Kaloyan Kolev <kaloyan21kolev@gmail.com>
2023-06-16 12:50:21 +0300

    Setup handlebars views and engine

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ const PORT = 5000
 const app = express();
 const routes = require('./routes');
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}))
+app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }))
 app.use(routes)
 
-app.get('/', (req, res) => {
-    res.send('first action');
-})
+
 
 app.listen(PORT, console.log(`Server is listening on port ${PORT}...`))
