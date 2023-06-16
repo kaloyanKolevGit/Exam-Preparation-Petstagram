f144360b8a5d519b0596135105864e657e970f22
Kaloyan Kolev <kaloyan21kolev@gmail.com>
2023-06-16 13:08:27 +0300

    Fixed static and tested home view

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const PORT = 5000
 const app = express();
 const routes = require('./routes');
+const handlebars = require('express-handlebars');
+const path = require('path');
 
 app.engine('hbs', handlebars.engine({
     extname: 'hbs'
 }))
 app.set('view engine', 'hbs')
-app.use(express.static('public'))
+app.set('views', 'src/views')
+app.use(express.static(path.resolve(__dirname, 'public')))
 app.use(express.urlencoded({ extended: false }))
 app.use(routes)
 
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 
 
 router.get('/', (req, res) => {
-    res.send('first action');
+    res.render('home');
 })
 
 
