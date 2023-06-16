2594bcd0bce063619f84d84482840abe070b905d
Kaloyan Kolev <kaloyan21kolev@gmail.com>
2023-06-16 12:41:56 +0300

    Setup routers and middlewares

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const PORT = 5000
 const app = express();
+const routes = require('./routes');
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+app.use(routes)
 
 app.get('/', (req, res) => {
     res.send('first action');
